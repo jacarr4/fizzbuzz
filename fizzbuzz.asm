3ace4f5c9b19bab4333630166c85c978ed651348
Jacob Carr <jacarr4@illinois.edu>
2018-02-16 02:16:25 -0600

    cleaned up, added comments

--- fizzbuzz.asm
+++ fizzbuzz.asm
@@ -1,11 +1,15 @@
+%define SYSCALL_WRITE 1
+%define SYSCALL_EXIT  60
+%define STDOUT_FD     1
+%define MAX_NUM       100
+%define LEN_FIZZ      4
+%define LEN_BUZZ      4
+%define LEN_NEWLINE   1
+
 section .data
     fizz    db   "Fizz"
     buzz    db   "Buzz"
     newline db   0xA
-    max_num db   100
-    err_msg db   "Please enter a number 0-9."
-    ascii_zero db 0x30
-    ascii_nine db 0x39
 
 section .text
     global _start
@@ -13,32 +17,35 @@ section .text
 
 
 _start:
-    push    rbp
+    push    rbp               ; set up stack frame
     mov     rbp, rsp
 
-    mov     rax, 1 
-    mov     rcx, 1
-    mov     rdx, 1
+    mov     rax, 1            ; initialize counters to 1
+    mov     rcx, 1            ; rcx counts by three
+    mov     rdx, 1            ; rdx counts by five
     call    for_loop
 
 exit:
-    mov     rax, 60
+    mov     rax, SYSCALL_EXIT ; terminate the program
     mov     rdi, 0
     syscall
 
 
 
+
+
 for_loop:
-    push    rbp
+    push    rbp               ; set up stack frame
     mov     rbp, rsp
 
 loop_start:
-    mov     rbx, 100
+    mov     rbx, MAX_NUM      ; compare current counter with max
     cmp     rax, rbx
-    je      for_loop_exit
-    cmp     rcx, 3
+    je      for_loop_exit     ; if equal, exit the loop
+
+    cmp     rcx, 3            ; compare the three counter with three
     jne     check_5
-    push    rax
+    push    rax               ; if equal, save counters and print "Fizz"
     push    rcx
     push    rdx
     call    print_fizz
@@ -47,9 +54,9 @@ loop_start:
     pop     rax
 
 check_5:
-    cmp     rdx, 5
+    cmp     rdx, 5            ; compare the five counter with five
     jne     new_line
-    push    rax
+    push    rax               ; if equal, save counters and print "Buzz"
     push    rcx
     push    rdx
     call    print_buzz
@@ -58,23 +65,26 @@ check_5:
     pop     rax
 
 new_line:
-    push    rax
+    push    rax               ; save counters and print newline
     push    rcx
     push    rdx
     call    print_newline
     pop     rdx
     pop     rcx
     pop     rax
-    inc     rax
+
+    inc     rax               ; increment counters
     inc     rcx
     inc     rdx
-    cmp     rcx, 4
-    jne     check_rdx
+
+check_rcx:
+    cmp     rcx, 4            ; if our three counter goes above three,
+    jne     check_rdx         ; then set it back to one
     mov     rcx, 1
 
 check_rdx:
-    cmp     rdx, 6
-    jne     continue_loop
+    cmp     rdx, 6            ; if our five counter goes above five,
+    jne     continue_loop     ; then set it back to one
     mov     rdx, 1
 
 continue_loop:
@@ -87,50 +97,58 @@ for_loop_exit:
 
 
 
+
+
+; print_fizz()
+; This function prints the string "Fizz" to stdout
 print_fizz:
-    push    rbp
+    push    rbp                      ; set up stack frame
     mov     rbp, rsp
-    mov     rax, 1
-    mov     rdi, 1
-    mov     rsi, fizz
-    mov     rdx, 4
+
+    mov     rax, SYSCALL_WRITE       ; syscall number in rax
+    mov     rdi, STDOUT_FD           ; file descriptor in rdi
+    mov     rsi, fizz                ; ptr to buffer in rsi
+    mov     rdx, LEN_FIZZ            ; number of bytes in rdx
     syscall
-    leave
+
+    leave                            ; tear down stack
     ret
 
 
 
+
+
+; print_buzz()
+; This function prints the string "Buzz" to stdout
 print_buzz:
-    push    rbp
+    push    rbp          ; same as print_fizz
     mov     rbp, rsp
-    mov     rax, 1
-    mov     rdi, 1
+
+    mov     rax, SYSCALL_WRITE
+    mov     rdi, STDOUT_FD
     mov     rsi, buzz
-    mov     rdx, 4
+    mov     rdx, LEN_BUZZ
     syscall
+
     leave
     ret
 
 
 
+
+
+; print_newline()
+; This function prints the newline character to stdtout
 print_newline:
-    push    rbp
+    push    rbp         ; same as print_fizz
     mov     rbp, rsp
-    mov     rax, 1
-    mov     rdi, 1
+
+    mov     rax, SYSCALL_WRITE
+    mov     rdi, STDOUT_FD
     mov     rsi, newline
-    mov     rdx, 1
+    mov     rdx, LEN_NEWLINE
     syscall
+
     leave
     ret
 
-print_invalid_input:
-    push    rbp
-    mov     rbp, rsp
-    mov     rax, 1
-    mov     rdi, 1
-    mov     rsi, err_msg
-    mov     rdx, 26
-    syscall
-    leave
-    ret
